1d119092f0b6f889e8e5ac86ecd3bf278d7e2307
Justin Wasserstein <justin.wasserstein@gmail.com>
2020-11-08 16:29:53 +0000

    Add cors middleware

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ require('dotenv').config();
 const express = require('express'),
 	  app     = express(),
 	  bodyParser = require('body-parser'),
+	  cors       = require('cors'),
 	  authRoutes = require('./routes/auth'),
 	  transactionRoutes = require('./routes/transactions');
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/transactions/:userId', transactionRoutes);
