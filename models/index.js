4ee705beef6b9ea76d36add690861b788896245d
Justin Wasserstein <justin.wasserstein@gmail.com>
2020-11-15 01:28:13 +0000

    Rename project to bank-of-wasserstein

--- models/index.js
+++ models/index.js
@@ -2,6 +2,6 @@ const mongoose = require('mongoose'),
 	  Users     = require('./users'),
 	  Transactions = require('./transactions');
 
-mongoose.connect('mongodb://localhost/bofa-clone', {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, keepAlive: true});
+mongoose.connect('mongodb://localhost/bank-of-wasserstein', {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, keepAlive: true});
 
 module.exports = {Users, Transactions};
