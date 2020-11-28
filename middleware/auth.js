92172598d0fa666ea047d7f7877e35a5a2bb84ed
Justin Wasserstein <justin.wasserstein@gmail.com>
2020-11-28 11:42:36 +0000

    Add check for presence of authorization header, change 'message' property of response json to 'error', and add return statements to next() calls

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,23 +1,26 @@
 const jwt = require('jsonwebtoken');
 
 function isUserLoggedIn(req, res, next){
+	if(!req.headers.authorization){
+		return res.status(401).json({error: 'Please log in first'});
+	}
 	jwt.verify(req.headers.authorization.split(' ')[1], process.env.SECRET_KEY, (err, decoded) => {
 		if(err) {
-			return res.status(401).json({message: 'Your token is invalid'});
+			return res.status(401).json({error: 'Your token is invalid'});
 		}
 		if(Date.now()/1000 - decoded.iat > 3600) {
-			return res.status(401).json({message: 'Your token has expired'});
+			return res.status(401).json({error: 'Your token has expired'});
 		}
-		next();
+		return next();
 	});
 }
 
 function doesUserOwnResource(req, res, next){
 	jwt.verify(req.headers.authorization.split(' ')[1], process.env.SECRET_KEY, (err, decoded) => {
 		if(decoded.id != req.params.userId){
-			return res.status(401).json({message: "You're not authorized to access that resource"});
+			return res.status(401).json({error: "You're not authorized to access that resource"});
 		}
-		next();
+		return next();
 	});
 }
 
