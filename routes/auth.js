b6f560dc97cd0d81518a660a9900a967e5a67825
Justin Wasserstein <justin.wasserstein@gmail.com>
2020-11-28 11:32:40 +0000

    Add checks for username, password, and email to auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -7,6 +7,16 @@ const express = require('express'),
 
 router.post('/signup', async function(req, res) {
 	try {
+		if(!req.body.username) {
+			return res.status(401).json({error: 'Username is required'});
+		}
+		if(!req.body.password) {
+			return res.status(401).json({error: 'Password is required'});
+		}
+		if(!req.body.email) {
+			return res.status(401).json({error: 'Email is required'});
+		}
+		
 		const user = await db.Users.create(req.body);
 		const token = jwt.sign({
 			id: user._id,
@@ -26,9 +36,16 @@ router.post('/signup', async function(req, res) {
 
 router.post('/signin', async function (req, res) {
 	try {
+		if(!req.body.username) {
+			return res.status(401).json({error: 'Username is required'});
+		}
+		if(!req.body.password) {
+			return res.status(401).json({error: 'Password is required'});
+		}
+		
 		const user = await db.Users.findOne({username: req.body.username});
 		if(!user){
-			return res.status(401).json({error: 'Invalid username/password'});
+			return res.status(401).json({error: "That username doesn't exist"});
 		}
 		const isMatch = await bcrypt.compare(req.body.password, user.password);
 		if(isMatch){
@@ -44,7 +61,7 @@ router.post('/signin', async function (req, res) {
 				token
 			});
 		} else {
-			res.status(401).json({error: 'Invalid username/password'});
+			res.status(401).json({error: 'Incorrect password'});
 		}
 	} catch (err) {
 		res.status(500).json({error: err.message});
