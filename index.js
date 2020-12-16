c13d04eab72949ac38a8a64536f5e8e8e457419e
justinwasserstein <justin.wasserstein@gmail.com>
2020-12-16 15:32:47 -0500

    Replace hard-coded port 3001 with process.env.PORT

--- index.js
+++ index.js
@@ -15,4 +15,5 @@ app.use(function(req, res, next) {
 	return res.status(404).json({error: 'Route not found'});
 });
 
-app.listen(3001, () => console.log('Listening on port 3001'));
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
