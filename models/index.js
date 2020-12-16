24e4664f8645cd368ac69fdde4ebe49330629666
justinwasserstein <justin.wasserstein@gmail.com>
2020-12-16 15:25:24 -0500

    Add process.env.MONGODB_URI to models/index.js

--- models/index.js
+++ models/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose'),
 	  Users     = require('./users'),
 	  Transactions = require('./transactions');
 
-mongoose.connect('mongodb://localhost/bank-of-wasserstein', {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, keepAlive: true});
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/bank-of-wasserstein', {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, keepAlive: true});
 mongoose.set('useCreateIndex', true);
 
 module.exports = {Users, Transactions};
